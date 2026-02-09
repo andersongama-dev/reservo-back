095005e9d9ae0c4d33dc06279a308ce4028324be
andersongama-dev <andersongamasilva5@gmail.com>
2026-02-09 12:28:51 -0300

    Testing success

--- app/models/user.ts
+++ app/models/user.ts
@@ -29,8 +29,5 @@ export default class User extends compose(BaseModel, AuthFinder) {
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   declare updatedAt: DateTime | null
 
-  @column()
-  declare role: string
-
   static accessTokens = DbAccessTokensProvider.forModel(User)
 }
